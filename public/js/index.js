90066b5447426c0db8e6405cd18bff30e7d6281c
Administrator <3173624088@qq.com>
2017-05-13 16:02:17 +0800

    小白的修改

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,11 @@
 
 $(function(){
 	var task_menu=$(".task_menu_list ul li");
+
     task_menu.on("click",function(){
 		$(this).addClass("active").siblings().removeClass("active");
+       // $(this).siblings().find("a").css("color","black");
+        //$(this).find("a").addClass("actived");
 	});
 })
 
@@ -30,12 +33,18 @@ function getStyle(obj,attr){
 
 function showPic(id){
 	var tm=document.getElementById("task-img");
-	tm.setAttribute("src","../public/images/img_"+id+".png");
+    var menu_ul=document.getElementsByClassName("task_menu_list")[0];
+    tm.setAttribute("src","../public/images/img_"+id+".png");
     for(var i=1;i<=5;i++){
         if(i==id){
             document.getElementsByClassName("text_c"+id)[0].style.display="block";
+           var lisa=menu_ul.getElementsByTagName("li")[id-1].getElementsByTagName("a")[0];
+            lisa.style.color="#EA5E43";
         }else{
             document.getElementsByClassName("text_c"+i)[0].style.display="none";
+            var lisa=menu_ul.getElementsByTagName("li")[i-1].getElementsByTagName("a")[0];
+            lisa.style.color="black";
+
         }
     }
 }
